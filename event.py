b78ebdca3232785eee0b3fb621315117e9272c2a
Jason Priem <jp@jasonpriem.org>
2017-08-24 20:51:18 -0700

    wiki event reduction working on first test.

--- event.py
+++ event.py
@@ -1,17 +1,19 @@
 
-def create_event(ced_event):
-    event = Event(ced_event)
-    return event
 
 
 class Event(object):
     def __init__(self, ced_event):
         self.ced_event = ced_event
+        self.subj_id = ced_event["subj_id"]
 
+    def get(self):
+        # override for events that req
+        pass
 
     @property
     def occurred_at(self):
         return self.ced_event["occurred_at"]
+
 
     def to_dict(self):
         try:
@@ -30,16 +32,39 @@ class Event(object):
         }
 
 
-class ExtantWikipediaEvent(Event):
-    def __init__(self):
-        self.page_id = None
-        self.events = []
+class WikipediaPageEvent(Event):
+    # https://www.eventdata.crossref.org/guide/sources/wikipedia/
 
-    def add(self, page_id, event):
-        self.page_id = page_id
-        self.events.append(event)
+    # the CED model counts many events that happen on a given wiki page.
+    # we just want one event to represent one page.
+    # the age of the event should be the initial creation of the page.
+    # for now we do not handle deleted pages or deleted references.
+
+    def is_same_page_as(self, test_url):
+        return self._find_stem(test_url) == self._find_stem(self.subj_id)
+
+    def _find_stem(self, url):
+        return url.split("&oldid=")[0]
+
+
+    def add_ced_event(self, ced_event):
+
+        # we are not interested in "replaces" or "is_version_of" for now.
+        if ced_event["relation_type_id"] != "references":
+            return False
+
+        # if this CED event happened earlier than the one we have already, replace it.
+        # we want this event to
+        if self.ced_event["occurred_at"] > ced_event["occurred_at"]:
+            self.ced_event = ced_event
+            return True
+
+        # otherwise, we are not interested in this new event.
+        return False
+
 
     def to_dict(self):
-        events = sorted(self.events, key=lambda x: x["date"])
-
+        ret = super(WikipediaPageEvent, self).to_dict()
+        ret["page_url"] = self._find_stem(self.subj_id)
+        return ret
 
--- source.py
+++ source.py
@@ -1,12 +1,13 @@
 from collections import defaultdict
-from event import create_event
+from event import Event
+from event import WikipediaPageEvent
 
 
 
 
 def make_event_source(id):
     if id == "wikipedia":
-        return ExtantWikipediaEventSource(id)
+        return WikipediaEventSource(id)
 
     else:
         return EventSource(id)
@@ -20,9 +21,10 @@ class EventSource(object):
         self.events_count_by_day = []
 
     def add_event(self, ced_event):
-        my_event = create_event(ced_event)
+        my_event = Event(ced_event)
         self.events.append(my_event)
         self._update()
+        return my_event
 
     def _update(self):
         # sort events by time
@@ -45,13 +47,36 @@ class EventSource(object):
 
 
 
-class ExtantWikipediaEventSource(EventSource):
-    pass
+class WikipediaEventSource(EventSource):
+    # https://www.eventdata.crossref.org/guide/sources/wikipedia/
+
+    # We need to add wiki events differently, because in this case a
+    # single Event in our model rolls up a whole bunch of CED events.
+    # We want just one Event for every wiki page, and they have oodles.
 
 
+    def _get_base_wiki_page_for_url(self, url):
+        for my_event in self.events:
+            if my_event.is_same_page_as(url):
+                return my_event
+
+        return None
 
 
+    def add_event(self, ced_event):
+        event_for_this_wiki_page = self._get_base_wiki_page_for_url(ced_event["subj_id"])
 
+        # first we see if this wiki page already has an Event
+        try:
+            event_for_this_wiki_page.add_ced_event(ced_event)
+
+        # if not, we creat a new Event for this wiki page
+        except AttributeError:
+            event_for_this_wiki_page = WikipediaPageEvent(ced_event)
+            self.events.append(event_for_this_wiki_page)
+
+        self._update()
+        return event_for_this_wiki_page
 
 
 
